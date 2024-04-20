865194ff3367e398edf0c3acda8ce7a4184c8dc8
ByakkoLord <g.fonsecadacosta@gmail.com>
2024-04-20 17:33:12 -0300

    add trancend icon in taskbar

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,9 +11,18 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Image icon = new Image("file:src/main/resources/com/example/demo/assets/logo.png");
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1180, 600);
-        stage.setTitle("Hello!");
+
+        stage.getIcons().add(icon);
+
+        stage.initStyle(javafx.stage.StageStyle.UNDECORATED);
+
+        stage.setMaxHeight(600);
+        stage.setMaxWidth(1180);
+        stage.setTitle("Transend");
         stage.setScene(scene);
         stage.show();
     }
