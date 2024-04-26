cb904e27da85cf7b3b76525f559d5556de12a203
ByakkoLord <g.fonsecadacosta@gmail.com>
2024-04-26 17:08:25 -0300

    new constructor

--- src/main/java/com/example/demo/Database.java
+++ src/main/java/com/example/demo/Database.java
@@ -5,16 +5,10 @@ import java.util.Arrays;
 
 
 public class Database {
-    private String url;
-    private String user;
-    private String password;
 
     private Connection connection;
 
     public Database(String url, String user, String password) {
-        this.url = url;
-        this.user = user;
-        this.password = password;
 
         try {
             this.connection = DriverManager.getConnection(url, user, password);
