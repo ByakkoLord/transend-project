d6fdc6705ac00645dcf368f65e52843e4f0a6b55
Deepsn <75620590+Deepsn@users.noreply.github.com>
2024-05-05 18:21:56 -0300

    chore: remove logging from spamming on the terminal

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,7 +1,18 @@
 package com.example.demo;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 public class Main {
     public static void main(String[] args) {
+        System.setOut(new PrintStream(new OutputStream() {
+            @Override
+            public void write(int arg0) throws IOException {
+
+            }
+        }));
+
         TransendApplication.main(args);
     }
 }
